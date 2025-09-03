be01461eb0da7ce904ce2b24405e89e262178f3e
Andre Beolchi <andregarbini2001@gmail.com>
2025-09-03 13:04:07 -0300

    fix(config): refactor configuration to use dotenv and improve type safety

--- src/infra/config.ts
+++ src/infra/config.ts
@@ -1,17 +1,39 @@
-export const config = {
-  env: process.env?.NODE_ENV ?? 'development',
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+interface Config {
+  env: 'development' | 'production' | 'test'
   otp: {
-    length: process.env?.OTP_LENGTH ? +process.env?.OTP_LENGTH : 6,
-    expirationInMs: process.env?.OTP_EXPIRATION_IN_MS ? +process.env?.OTP_EXPIRATION_IN_MS : 5 * 60 * 1000,
-  },
-  port: process.env?.PORT ?? 3000,
+    length: number
+    expirationInMs: number
+  }
+  port: number | string
   twilio: {
-    accountSid: process.env?.TWILIO_ACCOUNT_SID ?? '',
-    authToken: process.env?.TWILIO_AUTH_TOKEN ?? '',
-    phoneNumber: process.env?.TWILIO_PHONE_NUMBER ?? '',
+    accountSid: string
+    authToken: string
+    phoneNumber: string
+  }
+  google: {
+    email: string
+    password: string
+  }
+}
+
+export const config: Config = {
+  env: (process.env.NODE_ENV as Config['env']) ?? 'development',
+  otp: {
+    length: process.env.OTP_LENGTH ? +process.env.OTP_LENGTH : 6,
+    expirationInMs: process.env.OTP_EXPIRATION_IN_MS ? +process.env.OTP_EXPIRATION_IN_MS : 5 * 60 * 1000,
+  },
+  port: process.env.PORT ?? 3000,
+  twilio: {
+    accountSid: process.env.TWILIO_ACCOUNT_SID ?? '',
+    authToken: process.env.TWILIO_AUTH_TOKEN ?? '',
+    phoneNumber: process.env.TWILIO_PHONE_NUMBER ?? '',
   },
   google: {
-    email: process.env?.GOOGLE_EMAIL ?? '',
-    password: process.env?.GOOGLE_PASSWORD ?? '',
+    email: process.env.GOOGLE_EMAIL ?? '',
+    password: process.env.GOOGLE_PASSWORD ?? '',
   },
 }
